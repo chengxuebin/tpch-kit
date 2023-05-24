a655220c97c6e5b18861ab275441f06047082b25
Jonathan <chengxuebin@outlook.com>
2023-05-24 23:50:33 +0800

    更新 load.sh 输出文件路径

--- dbgen/patches/mysql/load.sh
+++ dbgen/patches/mysql/load.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-file="loaddata.sql"
+dir="$1"
+
+file="$dir/loaddata.sql"
 
 if [ ! -f "$file" ] ; then
     touch "$file"
 fi
 
-echo 'USE tpch;' >> "$file"
+echo 'USE tpch;' > "$file"
 echo 'SET FOREIGN_KEY_CHECKS=0;' >> "$file"
 
-dir="$1"
 for tbl in "$dir"/*.tbl; do
     table=$(basename "${tbl%.*}")
     echo "LOAD DATA LOCAL INFILE '$tbl' INTO TABLE $table" >> "$file"
