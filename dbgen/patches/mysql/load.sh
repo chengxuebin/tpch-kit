e17cda8f07a1188432f7467d76f48d91a46c037f
Jonathan <chengxuebin@outlook.com>
2023-05-25 14:35:47 +0800

    MySQL 行结尾去除|符号

--- dbgen/patches/mysql/load.sh
+++ dbgen/patches/mysql/load.sh
@@ -14,7 +14,7 @@ echo 'SET FOREIGN_KEY_CHECKS=0;' >> "$file"
 for tbl in "$dir"/*.tbl; do
     table=$(basename "${tbl%.*}")
     echo "LOAD DATA LOCAL INFILE '$tbl' INTO TABLE $table" >> "$file"
-    echo "FIELDS TERMINATED BY '|' LINES TERMINATED BY '|\n';" >> "$file"
+    echo "FIELDS TERMINATED BY '|' LINES TERMINATED BY '\n';" >> "$file"
 done
 echo 'SET FOREIGN_KEY_CHECKS=1;' >> "$file"
 
